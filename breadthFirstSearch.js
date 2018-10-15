2b27aed35fd4f09f5baaae79923c03aac8c3353a
Shahrukh <jalilshahrukh@gmail.com>
2018-10-15 11:37:21 -0400

    Breadth First Search

---
+++ breadthFirstSearch.js
@@ -0,0 +1,17 @@
+/*      10 
+      6   15 
+    3  8    20
+ROOT -> LEFT -> RIGHT
+QUEUE: [10, 6, 15, 3, 8, 20]
+*/
+function BFS() { 
+  var data = [], queue = [], node = this.root; 
+  if (node) queue.push(node); 
+  while (queue.length) { 
+    node = queue.shift(); 
+    data.push(node); 
+    if (node.left) queue.push(node.left); 
+    if (node.right) queue.push(node.right);
+  }
+  return data; 
+}
